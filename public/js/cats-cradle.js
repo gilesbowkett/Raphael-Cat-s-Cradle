f1c008fed7bfd1e062951821c325236041965097
Giles Bowkett <gilesb@gmail.com>
2010-10-16 11:47:10 -0700

    successfully drawing and reporting on X and Y...
    however the X and Y are kinda stupid fucked. it gives you the X/Y
    of the bounding box, which is apparently keyed to the edge instead
    of the center, so you have to do some math jiggerypokery to get the
    actual center X/Y.

--- public/js/cats-cradle.js
+++ public/js/cats-cradle.js
@@ -1,4 +1,18 @@
-window.onload = function() {  
-  var paper = new Raphael(document.getElementById('leonardo'), 500, 500);
-  var circle = paper.circle(100, 100, 80);
+function rand_up_to(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+window.onload = function() {
+  var numberBalls = 5;
+  var balls = [];
+  var circle;
+  var minimumDistance = 100;
+
+  var paper = new Raphael(document.getElementById('leonardo'), 1000, 1000);
+
+  for (var i = 0; i < numberBalls; i++) {
+    circle = paper.circle(20 + rand_up_to(300), 20 + rand_up_to(300), 20);
+    circle.attr({fill: '#330099'});
+    console.log("circle drawn at " + circle.getBBox().x + "/" + circle.getBBox().y);
+  }
 }
