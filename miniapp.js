0ca39f2d97791efa10549409bf646882cd36cbc2
Giles Bowkett <gilesb@gmail.com>
2010-10-16 10:35:20 -0700

    OK, this much works

--- miniapp.js
+++ miniapp.js
@@ -6,21 +6,10 @@ var http = require('http'),
     jade = require('./vendor/jade');
 
 http.createServer(function (request, response) {
- // sys.puts("wtf");
- // var file = new nodeStatic.Server('./public', {
-       // cache: false
-     // }),
-   //   location = url.parse(request.url, true),
- //     params = (location.query || request.headers);
- // if (location.pathname == '/' && request.method == "GET") {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    jade.renderFile('view.jade', {}, function(err, html){
-      response.end(html);
-    });
- // }
- // else {
-  //   file.serve(request, response);
-  //}
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  jade.renderFile('view.jade', {}, function(err, html){
+    response.end(html);
+  });
 }).listen(3000, "127.0.0.1");
 
 
